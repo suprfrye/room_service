0db16c751978f964c925c5443e6df2971e416233
Max Rater <msrater@gmail.com>
2015-03-18 19:51:17 -0400

    lightsaber

--- Gemfile
+++ Gemfile
@@ -9,3 +9,8 @@ end
 group :development do
     gem 'shotgun'
 end
+
+gem 'rake'
+gem 'activesupport', '~>4.1'
+gem 'activerecord', '~>4.1'
+gem 'pg'
--- environment.rb
+++ environment.rb
@@ -8,9 +8,17 @@ require 'rubygems'
 require 'uri'
 require 'pathname'
 
+require 'active_record'
+require 'pg'
+
+
+
 
  APP_ROOT = Pathname.new(File.expand_path('../', __FILE__))
  APP_NAME = APP_ROOT.basename.to_s
 
+Dir[APP_ROOT.join('app', 'models', '*.rb')].each { |file| require file }
  #Load Controllers
  Dir[APP_ROOT.join('app', 'controllers', '*.rb')].each { |file| require file }
+
+require APP_ROOT.join('database')
