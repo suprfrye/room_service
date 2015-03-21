4551238453da887cc0bad81376215383d6b3a269
Eveanandi Butler <butler.eveanandi@gmail.com>
2015-03-21 15:29:45 -0400

    Create form for new user input

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -1,5 +1,9 @@
 require 'sinatra'
 
+get '/users/new' do
+  erb :"users/new"
+end
+
 get '/users/:id' do
   @current_user = User.find_by(id: params[:id])
   "#{@current_user.first_name}"
@@ -11,11 +15,18 @@ get '/users/:id' do
 #   end
 end
 
-get '/users/new' do
-  erb :"users/new"
-end
 
 post '/users' do
-  new_user = User.create
+  new_user = User.new(first_name: params[:first_name],
+                        last_name: params[:last_name],
+                        email_address: params[:email_address],
+                        position: params[:position],
+                        password: params[:password]
+                         )
 
+  if new_user.save
+    redirect "/users/#{new_user.id}"
+  else
+    [402, "Sorry, Something went wrong"]
+  end
 end
