3a94430682ebff2e4a190f5b0e7aba7051db5f76
Max Rater <msrater@gmail.com>
2015-03-22 19:58:46 -0400

    Final

--- app/controllers/rooms.rb
+++ app/controllers/rooms.rb
@@ -3,17 +3,19 @@ require 'shotgun'
 
 get '/rooms' do
 @rooms = Room.all
+@time = Room.find_by(id: params[:group_id])
 
 erb :'rooms/index'
 end
 
-get '/rooms/:id' do
-@room = Room.find_by(id: params[:id])
-  if @room
-  erb :"rooms/show"
-  else
-  [404, 'No post found']
-  end
-end
+# get '/rooms/:id' do
+# @room = Room.find_by(id: params[:id])
+#   if @room
+#   erb :"rooms/show"
+#   else
+#   [404, 'No post found']
+#   end
+
+# end
 
 
