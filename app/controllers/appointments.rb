543819c919e92e42fb39b2fba27278ce8753c32e
suprfrye <tara.c.frye@gmail.com>
2015-03-21 20:03:08 -0400

    Add appointments edit routes and erb file

--- app/controllers/appointments.rb
+++ app/controllers/appointments.rb
@@ -17,3 +17,21 @@ post '/users/:id/appointments/new' do
     [404, "Invalid Appointment Entry"]
   end
 end
+
+get '/users/:id/appointments/:appt_id/edit' do
+  @appt = Appointment.find(params[:appt_id])
+  erb :"/appointments/edit"
+end
+
+put '/users/:id/appointments/:appt_id/edit' do
+  cur_appt = Appointment.find(params[:appt_id])
+  cur_appt.update_attributes({group: Group.find_by(name: params[:group]),
+              room: Room.find_by(name: params[:room]),
+              start_datetime: DateTime.parse("#{params[:date]} #{params[:start_datetime]}"),
+              end_datetime: DateTime.parse("#{params[:date]} #{params[:end_datetime]}")})
+  if cur_appt.save
+    redirect "/users/#{params[:id]}"
+  else
+    [404, "Invalid Appointment Entry"]
+  end
+end
