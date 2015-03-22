5fc2b745a0aebe14d6bacadb17618c104c53af72
Max Rater <msrater@gmail.com>
2015-03-22 18:42:18 -0400

    Created edit for groups

--- app/controllers/groups.rb
+++ app/controllers/groups.rb
@@ -17,15 +17,40 @@ get '/users/:id/groups/new' do
 
 end
 
+get '/users/:id/groups/:group_id/edit' do
+
+  #@user = User.find_by(id: params[:id])
+  @list = User.all
+  @group = Group.find_by(id: params[:group_id])
+#  @group_members = @group.users.pluck(:id)
+
+
+  erb :"groups/edit"
+
+end
+
 post '/users/:id/groups/new' do
   # hacky thing...params[:id] disappears after group?
   user_id = params[:id]
 #if the group saves, this redirects to the appointment group
   group = Group.new(name: params[:name])
   list_user_id = params.keep_if { |k,v| v == 'on' }
-  group.users = list_user_id.keys.map! { |user_id| User.find_by(id: user_id) }
+  group.user = User.find_by(id: user_id)
+  group.users = list_user_id.keys.map! { |id| User.find_by(id: id) }
   group.save
 
 redirect "/users/#{user_id}/appointments/new"
+
+end
+
+put '/users/:id/groups/:group_id/edit' do
+  user_id = params[:id]
+  group = Group.find_by(id: params[:group_id])
+  list_user_id = params.keep_if { |k,v| v == 'on' }
+  group.user = User.find_by(id: user_id)
+  group.users = list_user_id.keys.map! { |id| User.find_by(id: id) }
+  group.save
+
+  redirect "/users/#{user_id}/appointments/new"
 
 end
