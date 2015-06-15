a15410f37d8478df70f7f9b0ee963c618a1e5c9e
suprfrye <tara.c.frye@gmail.com>
2015-06-15 17:34:14 -0400

    Remove comments and newlines

--- app/controllers/groups.rb
+++ app/controllers/groups.rb
@@ -1,38 +1,19 @@
 require 'sinatra'
-# require 'shotgun'
-
-# get '/groups' do
-# @groups = Group.all
-
-# erb :'groups/index'
-# end
-
 
 get '/users/:id/groups/new' do
-
-  #@user = User.find_by(id: params[:id])
   @list = User.all
-
   erb :"groups/new"
-
 end
 
 get '/users/:id/groups/:group_id/edit' do
-
-  #@user = User.find_by(id: params[:id])
   @list = User.all
   @group = Group.find_by(id: params[:group_id])
-#  @group_members = @group.users.pluck(:id)
-
-
   erb :"groups/edit"
-
 end
 
 post '/users/:id/groups/new' do
-  # hacky thing...params[:id] disappears after group?
   user_id = params[:id]
-#if the group saves, this redirects to the appointment group
+  # if the group saves, this redirects to the appointment group
   group = Group.new(name: params[:name])
   list_user_id = params.keep_if { |k,v| v == 'on' }
   group.user = User.find_by(id: user_id)
@@ -40,7 +21,6 @@ post '/users/:id/groups/new' do
   group.save
 
   redirect "/users/#{user_id}/appointments/new"
-
 end
 
 put '/users/:id/groups/:group_id/edit' do
@@ -52,5 +32,4 @@ put '/users/:id/groups/:group_id/edit' do
   group.save
 
   redirect "/users/#{user_id}/appointments/new"
-
 end
