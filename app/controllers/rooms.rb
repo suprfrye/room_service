5c8b24f931643aca272413fb0871e211b0c4bf5d
suprfrye <tara.c.frye@gmail.com>
2015-06-15 17:36:11 -0400

    Indent rooms controller get route block

--- app/controllers/rooms.rb
+++ app/controllers/rooms.rb
@@ -1,8 +1,7 @@
 require 'sinatra'
 
 get '/users/:id/rooms' do
-@rooms = Room.all
-@time = Room.find_by(id: params[:group_id])
-
-erb :'rooms/index'
+  @rooms = Room.all
+  @time = Room.find_by(id: params[:group_id])
+  erb :'rooms/index'
 end
